a97f00b4c3753836bdda9cbd09c5ffee2acf7c1d
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-10 21:05:51 -0500

    Cloud functions with Random Number

---
+++ cloud-functions/functions/src/index.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.random = functions.https.onRequest((req, res) => {
+    const randomNumber = Math.random();
+    res.status(200).send(`
+        <!doctype html>
+            <head>
+                <title>Random Number</title>
+            </head>
+            <body>
+                ${'Your number is: ' + randomNumber}
+            </body>
+        </html>`);
+});
